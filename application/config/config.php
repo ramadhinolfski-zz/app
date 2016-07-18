4fd27d32854cb4aa6ccfa5b6f1e86ca99be8b4c3
ramadhinolfski <hanafiah.ramadhan@gmail.com>
2016-07-18 20:30:11 +0700

    Set dinamic base_url

--- application/config/config.php
+++ application/config/config.php
@@ -1,7 +1,9 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-$config['base_url'] = 'http://localhost/app';
+$http = 'http' . ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') ? 's' : '') . '://';
+$newurl = str_replace("index.php", "", $_SERVER['SCRIPT_NAME']);
+$config['base_url'] = "$http" . $_SERVER['SERVER_NAME'] . "" . $newurl;
 
 /*
 |--------------------------------------------------------------------------
